6bf18cc70da04b5b975f94934fc80412bbe5cd54
wingedotter5 <108061368+wingedotter5@users.noreply.github.com>
2022-06-27 19:06:23 +0530

    Update RouteSwitch.js

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -5,7 +5,7 @@ import Products from "./pages/Products";
 
 const RouteSwitch = () => {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/shopping-cart">
       <CartProvider>
         <Routes>
           <Route path="/" element={<Home />} />
